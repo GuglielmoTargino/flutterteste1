d10e7599a22d267c6d82f72573fbf3880cfd55ef
GHT <guglielmohenriquestargino@gmail.com>
2026-01-20 20:24:55 -0300

    teste3

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,44 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyGuga());
+  runApp(const AppGuga());
 }
 
-class MyGuga extends StatelessWidget {
-  const MyGuga({super.key});
+class AppGuga extends StatelessWidget {
+  const AppGuga({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        colorScheme: .fromSeed(
-          seedColor: const Color.fromARGB(255, 205, 104, 36),
-        ),
-      ),
-
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: const PaginaPrincipal(titulo: 'Teste 1 Flutter'));
   }
 }
 
-/////////////////////////////////////
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PaginaPrincipal extends StatefulWidget {
+  const PaginaPrincipal({super.key, required this.titulo});
   // This widget is the home page of your application. It is stateful, meaning
-  final String title;
+  final String titulo;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PaginaPrincipal> createState() => _MyHomePageState();
 }
-////////////////////////////////////////////////
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<PaginaPrincipal> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // TY THIS: Try changing the color here to a specific color (to
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the PaginaPrincipal object that was created by
+        //title: Text(widget.titulo),
+        title: Text("widget.titulo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
   }
 }
