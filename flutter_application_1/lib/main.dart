d9345cafd8cfba61ef4baa6c4d706f1b7da3ca35
GHT <guglielmohenriquestargino@gmail.com>
2026-01-20 21:39:23 -0300

    segue

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,83 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AppGuga());
+  runApp(const MeuApp());
 }
 
-class AppGuga extends StatelessWidget {
-  const AppGuga({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: const PaginaPrincipal(titulo: 'Teste 1 Flutter'));
-  }
-}
-
-class PaginaPrincipal extends StatefulWidget {
-  const PaginaPrincipal({super.key, required this.titulo});
-  // This widget is the home page of your application. It is stateful, meaning
-  final String titulo;
-
-  @override
-  State<PaginaPrincipal> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<PaginaPrincipal> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      _counter--;
-    });
-  }
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
     return Scaffold(
-      appBar: AppBar(
-        // TY THIS: Try changing the color here to a specific color (to
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the PaginaPrincipal object that was created by
-        //title: Text(widget.titulo),
-        title: Text("widget.titulo"),
+      appBar: AppBar(title: const Text('Tela 2')),
+      body: const Center(
+        child: Text('Essa é a Tela 2', style: TextStyle(fontSize: 24)),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+    );
+  }
+}
 
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 👈 sem const
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Guga',
+                  style: TextStyle(fontSize: 32, color: Colors.white),
+                ),
+
+                const SizedBox(height: 12),
+
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Digite algo...',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+                const SizedBox(height: 12),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Tela2()),
+                    );
+                  },
+                  child: const Text('Ir para Tela 2'),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
+        ),
       ),
     );
   }
