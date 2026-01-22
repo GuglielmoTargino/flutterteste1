50bc6ec8b29c36740cf5add9c575155f83281e85
GHT <guglielmohenriquestargino@gmail.com>
2026-01-22 20:35:09 -0300

    codigo com rows

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
-import 'telas/tela1.dart';
 
 void main() {
-  runApp(const MeuApp());
+  runApp(const MyApp());
 }
 
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Tela1());
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          title: const Text(
+            'Exemplo com Rows',
+            style: TextStyle(color: Color.fromARGB(255, 40, 42, 167)),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Center(
+          child: Column(
+            // só pra empilhar as 3 Rows na vertical
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ===== ROW 1 =====
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.star),
+                  SizedBox(width: 10),
+                  Text('Row 1'),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              // ===== ROW 2 =====
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.home),
+                  Icon(Icons.favorite),
+                  Icon(Icons.settings),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              // ===== ROW 3 =====
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  ElevatedButton(onPressed: null, child: Text('OK')),
+                  SizedBox(width: 10),
+                  ElevatedButton(onPressed: null, child: Text('Cancelar')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
